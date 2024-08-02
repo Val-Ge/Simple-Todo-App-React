bbb42dc63d42bc606885d322d32fc446ff706a18
Val-Ge <vg.dataengineer@gmail.com>
2024-08-02 10:48:31 +0100

    add TodoList, TodoForm, TodoItem

---
+++ src/TodoList.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import List from '@mui/material/List';
+import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
+
+const initialTodos = [
+    {id: 1, text: "Walk the dog", completed: false},
+    {id: 2, text: "Walk the cat", completed: false},
+    {id: 3, text: "Walk the fish", completed: true},
+    {id: 4, text: "Walk the chickens", completed: false},
+];
+
+export default function TodoList() {
+    const [todos, setTodos] = useState(initialTodos);
+
+    const removeTodo = (id) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter((t) => t.id !== id);
+        });
+    }
+    const toggleTodo = (id) => {
+        setTodos(prevTodos => {
+            return prevTodos.map(todo => {
+                if(todo.id === id){
+                    return {...todo, completed: !todo.completed};
+                } else {
+                    return todo;
+                }
+            });
+        });
+
+    };
+    const addTodo = (text) => {
+        setTodos(prevTodos => {
+            return [...prevTodos, {text: text, id: 8, completed: false}]
+        })
+    }
+    return (
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+             {todos.map((todo) => (
+                <TodoItem 
+                todo={todo}
+                key={todo.id}
+                removeTodo={() => removeTodo(todo.id)}
+                toggle={() => toggleTodo(todo.id)}
+                />
+             ))}
+             <TodoForm addTodo={addTodo}/>
+        </List>
+    );
+}
